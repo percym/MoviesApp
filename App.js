5609792198b75b52506764fca661c102a8a417df
percym <percymdev@gmail.com>
2021-09-29 11:06:14 +0200

    adding screens

--- App.js
+++ App.js
@@ -3,13 +3,10 @@ import { Text, View } from 'react-native';
 import axios from 'axios';
 import { useState } from 'react'; 
 import { setState } from 'expect';
-import { useEffect } from 'react';  
+import { useEffect } from 'react';
+import { getPopularMovies } from './services/services';  
 
-const getPopularMovies = async()=>{
-  const resp = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=935665956143eb69d59d40362bab7a4d');
-  return resp.data.results;
-  
-}
+
 const App = () => {
   const [movie, setMovie] = useState('');
   const [error, setError] = useState('');
---
+++ services/services.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const apiUrl ='https://api.themoviedb.org/3';
+const apiKey ='api_key=935665956143eb69d59d40362bab7a4d';
+/**
+ * 
+ * @returns a list of popular movies
+ */
+export const getPopularMovies = async()=>{
+    const resp = await axios.get(
+            `${apiUrl}/movie/popular?${apiKey}`
+            );
+    return resp.data.results;
+    
+  }
+
+  /**
+ * 
+ * @returns a list of upcoming movies
+ */
+export const getUpcomingMovies = async()=>{
+    const resp = await axios.get(
+            `${apiUrl}/movie/upcoming?${apiKey}`
+            );
+    return resp.data.results;
+    
+  }
