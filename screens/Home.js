6b4449b100b1a417353f9bb43d2e6fdc75801779
percym <percymdev@gmail.com>
2021-09-30 16:00:07 +0200

    movie names are coming

--- screens/Home.js
+++ screens/Home.js
@@ -1,60 +1,79 @@
 import React from 'react';
-import { Text, View , Dimensions, StyleSheet} from 'react-native';
+import {Text, View, Dimensions, StyleSheet, FlatList} from 'react-native';
 import axios from 'axios';
-import { useState } from 'react'; 
-import { setState } from 'expect';
-import { useEffect } from 'react';
-import { getPopularMovies, getUpcomingMovies } from '../services/services';
+import {useState} from 'react';
+import {setState} from 'expect';
+import {useEffect} from 'react';
+import {getPopularMovies, getUpcomingMovies} from '../services/services';
 import {SliderBox} from 'react-native-image-slider-box';
 
-    const dimensions = Dimensions.get('screen');
-const Home =()=>{
-    const imageUrl = 'https://images.tmdb.org/t/p/w500';
-    const [moviesImages, setMoviesImages] = useState(['https://placeholder.com/']);
-    const [error, setError] = useState(false);
-  
-  
-    useEffect(() => {
-        getUpcomingMovies().then(movies=>{
+const dimensions = Dimensions.get('screen');
+const Home = () => {
+  const imageUrl = 'https://images.tmdb.org/t/p/w500';
+  const [moviesImages, setMoviesImages] = useState('');
+  const [popularMovies, setPopularMovies] = useState('');
+  const [error, setError] = useState(false);
 
-            const moviesImagesArray = [];
-            
-           
-            movies.forEach(movie => {
-                console.log('https://images.tmdb.org/t/p/w500'+ movie.poster_path);    
-            moviesImagesArray.push('https://images.tmdb.org/t/p/w500'+ movie.poster_path);
-            setMoviesImages(moviesImagesArray);
-        })
-        }).catch(err =>{
-            setError(err);
-        })
+  useEffect(() => {
+    getUpcomingMovies()
+      .then(movies => {
+        const moviesImagesArray = [];
 
-    
-    },[]);
+        movies.forEach(movie => {
+          console.log('https://images.tmdb.org/t/p/w500' + movie.poster_path);
+          moviesImagesArray.push(
+            'https://images.tmdb.org/t/p/w500' + movie.poster_path,
+          );
+          setMoviesImages(moviesImagesArray);
+        });
+      })
+      .catch(err => {
+        setError(err);
+      });
+  }, []);
 
-  
-    return(
-        <View style={styles.sliderContainer}>
-       
-        <SliderBox 
-          images={moviesImages} 
-          autoplay={true} circleLoop={true} 
+  getPopularMovies().then(movies=>{
+      setPopularMovies(movies);
+  }).catch(err=>{
+      setError(err);
+  })
+
+  return (
+    <React.Fragment>
+      <View style={styles.sliderContainer}>
+        <SliderBox
+          images={moviesImages}
+          autoplay={true}
+          circleLoop={true}
           dotStyle={styles.sliderStyle}
-          sliderBoxHeight={dimensions.height/1.5}></SliderBox>
-         </View>
-    );
-   
-}
+          sliderBoxHeight={dimensions.height / 1.5}></SliderBox>
+      </View>
+      <View style={styles.carousel}>
+        <FlatList 
+            data={popularMovies} 
+            horizontal={true}
+            renderItem={({item})=> <Text>{item.title}</Text>}>
+
+          </FlatList >
+      </View>
+
+    </React.Fragment>
+  );
+};
 const styles = StyleSheet.create({
-    sliderContainer:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-    },
-    sliderStyle:{
-        height:0
-    }
-})
+  sliderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sliderStyle: {
+    height: 0,
+  },
+  carousel: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-    export default Home;
-
+export default Home;
